349da505d50f400ba0cbacf4b446c3f1025a2368
Nano <voutsina.alejandro@gmail.com>
2026-05-26 03:39:23 -0300

    sprint 3 super terminado

--- src/app/environments/enviroments.ts
+++
@@ -1,6 +0,0 @@
-export const enviroments = {
-  production: false,
-  supabaseUrl: 'https://agyvrqfvvwacjvgvmaxg.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFneXZycWZ2dndhY2p2Z3ZtYXhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMzA5MjMsImV4cCI6MjA5NDgwNjkyM30.QikH07-vKeT3hgYW_UqtK_LfXRUsG1Mc9npSB5kxnzc',
-};
